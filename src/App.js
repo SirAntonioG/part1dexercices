043a54116d1b2abe226d6b57df1b3ec739dbcbda
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-09 20:30:08 -0500

    Fullstack part1 secciond exercises: 1.7 completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { useState } from 'react'
-const Button = ({onClick, text}) => {
+const Button = ({ onClick, text }) => {
   return (<button onClick={onClick}>
     {text}
   </button>)
 }
-const Title = ({text}) => (
+const Title = ({ text }) => (
   <h2>{text}</h2>
 )
-const Statistics = ({good,neutral,bad}) => {
-  console.log(good,neutral,bad)
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = ( good * 1 + neutral * 0 + bad * -1 ) / all;
   return (
     <p>
-      good {good}<br/>
-      neutral {neutral}<br/>
-      bad {bad}<br/>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}<br />
+      all {all}<br />
+      average {average}<br />
+      positive {100 * good / all} %<br />
     </p>
   )
 }
@@ -25,7 +29,7 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good +1);
+    setGood(good + 1);
   }
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
@@ -37,12 +41,12 @@ const App = () => {
 
   return (
     <div>
-      <Title text="give feedback"/>
+      <Title text="give feedback" />
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
       <Title text="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
